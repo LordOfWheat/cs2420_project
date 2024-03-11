ef1202f69e0146c8c349f95081ae9d9805ad4962
LordOfWheat <christinaroesner@mail.weber.edu>
2024-03-11 10:32:33 -0600

    add node

--- list.h
+++ list.h
@@ -5,6 +5,13 @@
 #ifndef CS2420_PROJECT_LIST_H
 #define CS2420_PROJECT_LIST_H
 
+template <class T>
+struct Node {
+    Node(T v):value(v),next(nullptr),prev(nullptr){}
+    T value;
+    Node<T>* next;
+    Node<T>* prev;
+};
 
 class list {
 
