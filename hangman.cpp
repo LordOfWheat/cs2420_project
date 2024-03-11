8d0dceb770c1d9690b71a0729d6b67aad996ccdb
LordOfWheat <christinaroesner@mail.weber.edu>
2024-03-11 10:26:58 -0600

    setup

--- hangman.cpp
+++ hangman.cpp
@@ -2,6 +2,7 @@
 // Created by christinaroesner on 3/11/2024.
 //
 #include <iostream>
+#include "list.h"
 using namespace std;
 
 int main() {
---
+++ list.h
@@ -0,0 +1,14 @@
+//
+// Created by christinaroesner on 3/11/2024.
+//
+
+#ifndef CS2420_PROJECT_LIST_H
+#define CS2420_PROJECT_LIST_H
+
+
+class list {
+
+};
+
+
+#endif //CS2420_PROJECT_LIST_H
