25674d548b4d9d4b75e03e63b3cc4f699d06a389
SyPhase <pi2568gamerz@gmail.com>
2024-03-13 12:33:47 -0600

    Update gamemanager.h

--- gamemanager.h
+++ gamemanager.h
@@ -10,10 +10,48 @@ enum GameState { Start, InProgress, End };
 class gamemanager
 {
 private:
-    GameState gameState = Start;
+    string word;
+    // Not needed?
+    //GameState gameState = GameState::Start;
 
 public:
-    //
+    GameState StartGame()
+    {
+        // Choose random word, set word
+        // cout << "_" for word.length
+        // Draw empty hangman (cout << hangman)
+
+        // Start next phase
+        return GameState::InProgress;
+    }
+
+    GameState InProgress()
+    {
+        // Prompt user for a letter, set letter
+        // Search each letter in word for letter
+        // if letter NOT found:
+            // Add letter to list of incorrect letters
+            // Increment Hangman state
+        // else if letter is found:
+            // Add letter to list of correct letters
+        // Check if you Win/Lose:
+        // (if letters == word or hangman state == max)
+            // Start End Phase
+        // Draw "_" for each letter not in correct letters list
+        // Draw current Hnagman State
+
+        // Restart In-Progress phase if NOT Win/Lose
+        return GameState::InProgress;
+    }
+
+    GameState EndGame()
+    {
+        // Show Win/Lose screen
+        // Wait for user input
+
+        // Restart Game
+        return GameState::Start;
+    }
 };
 
 #endif //CS2420_PROJECT_GAMEMANAGER_H
