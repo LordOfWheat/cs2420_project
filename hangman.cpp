e47cfe743775c362e9045d8f6758526a17f2b8d9
SyPhase <pi2568gamerz@gmail.com>
2024-04-17 13:10:51 -0600

    Added Game Logic loop to hangman.cpp

--- hangman.cpp
+++ hangman.cpp
@@ -4,39 +4,47 @@
 
 #include "list.h"
 #include "GameManager.h"
+
 using namespace std;
 
-int main() {
-     /*
-      * start screen
-      * 20 words stored in list (use the random function to pick random word)
-      * after random word is selected, store the letters in word separately in list (ex. [ c, a, t])
-      * make an empty list that will be for correct letters (semi-empty, fill it with underscores for printing)
-      * make an empty list that will be for incorrect letters
-      * print correct letters
-      * wait for user input
-      * search for inputted letter in the letter-in-word list
-      *     if not found, insert in incorrect letters, update hangman state (?) and print
-      *     if found, replace in correct letters; print correct letters (with the underscores) and same hangman frame
-      * repeat until either correct letters list has no underscores or hangman state is at max
-      * if no underscores, win screen
-      * if hangman state is max, lose screen
-      * when finished, clear lists
-      */
-
+int main()
+{
+    /*
+     * start screen
+     * 20 words stored in list (use the random function to pick random word)
+     * after random word is selected, store the letters in word separately in list (ex. [ c, a, t])
+     * make an empty list that will be for correct letters (semi-empty, fill it with underscores for printing)
+     * make an empty list that will be for incorrect letters
+     * print correct letters
+     * wait for user input
+     * search for inputted letter in the letter-in-word list
+     *     if not found, insert in incorrect letters, update hangman state (?) and print
+     *     if found, replace in correct letters; print correct letters (with the underscores) and same hangman frame
+     * repeat until either correct letters list has no underscores or hangman state is at max
+     * if no underscores, win screen
+     * if hangman state is max, lose screen
+     * when finished, clear lists
+     */
+    
     // for testing the functions, you may delete if you want
-     auto ListTest = new list<int>();
-     auto ListTest2 = new list<int>();
-     auto ListTest3 = new list<int>();
+//     auto ListTest = new list<int>();
+//     auto ListTest2 = new list<int>();
+//     auto ListTest3 = new list<int>();
+//
+//     for (int i = 1; i < 11; i++) {
+//         ListTest->insert(i);
+//         ListTest2->insert(i);
+//     }
+//    auto ListTest = new list<int>();
+//
+//    for (int i = 1; i < 11; i++)
+//    {
+//        ListTest->insert(i);
+//    }
 
-     for (int i = 1; i < 11; i++) {
-         ListTest->insert(i);
-         ListTest2->insert(i);
-     }
-
-     for (int i = 20; i < 30; i++) {
-         ListTest3->insert(i);
-     }
+//     for (int i = 20; i < 30; i++) {
+//         ListTest3->insert(i);
+//     }
 
 
 //     cout << "Before replace: " << endl;
@@ -47,6 +55,16 @@ int main() {
 //     cout << "After replace: " << endl;
 //     cout << *ListTest << endl << endl;
 
+//    cout << *ListTest << endl;
+//    cout << ListTest->search(3) << endl << endl;
+//
+//    ListTest->insert(3);
+//
+//    cout << *ListTest << endl;
+//    cout << ListTest->search(3, 2) << endl;
+//    cout << *ListTest << endl << endl;
+
+
 //     cout << *ListTest << endl;
 //     cout << ListTest->search(3) << endl << endl;
 //
@@ -56,12 +74,22 @@ int main() {
 //     cout << ListTest->search(3, 2) << endl;
 //     cout << *ListTest << endl << endl;
 
-        cout << ListTest->searchAll(ListTest2) << endl;
-        cout << ListTest->searchAll(ListTest3) << endl;
-     
-     
-     // Game Manager Testing
-//     GameManager gameManager;
-//
-//     gameManager.StartGame();
+//        cout << ListTest->searchAll(ListTest2) << endl;
+//        cout << ListTest->searchAll(ListTest3) << endl;
+    
+    
+    ///// --- Main Game Loop --- ////
+    GameManager gameManager;
+    
+    GameState gameState = Start;
+    
+    do // Game Loop, runs the game until finished
+    {
+        if (gameState == Start)
+            gameState = gameManager.StartGame();
+        else if (gameState == InProgress)
+            gameState = gameManager.InProgress();
+        else if (gameState == End)
+            gameState = gameManager.EndGame();
+    } while (gameState != End);
 }
