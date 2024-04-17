ddb8e97fc9bf6ca0d086a3a38cd5c8af8cff4f94
LordOfWheat <christinaroesner@mail.weber.edu>
2024-04-17 11:46:01 -0600

    searchAll //TODO

--- list.h
+++ list.h
@@ -30,7 +30,7 @@ class list {
         int get_count(); // get the count of the list
         int search(T data); // find index of letter
         int search(T data, int iter); // find index of (iter)st letter
-        bool searchAll(list<T> *l);
+        bool searchAll(string word);
         void insert(T data);
         void remove(T data);
         void replace(int index, T data); // to replace selected underscore with correct letter if used by player
@@ -42,7 +42,7 @@ class list {
         Node<T>* tail;
         int cnt;
 
-        bool searchAll(Node<T> *first, Node<T> *second);
+        bool searchAll(Node<T> *first, string word);
 };
 
 template <class T>
@@ -107,22 +107,38 @@ int list<T>::search(T data, int iter) {
 }
 
 template <class T>
-bool list<T>::searchAll(list<T> *l) {
-    return searchAll(this->head, l->head);
+bool list<T>::searchAll(string word) {
+    return searchAll(this->head, word);
 }
 
 template <class T>
-bool list<T>::searchAll(Node<T> *first, Node<T> *second) {
-    bool flag = true;
+bool list<T>::searchAll(Node<T> *first, string word) {
+    bool flag;
 
-    while (second) {
-        if (first->value != second->value) {
-            flag = false;
-            break;
+//    while (first && second) {
+//        if (first->value != second->value) {
+//            flag = false;
+//            break;
+//        }
+//
+//        first = first->next;
+//        second = second->next;
+//    }
+
+    for (int i = 0; i < word.length(); i++) {
+        flag = false;
+
+        while (first) {
+            if (first->value == word[i]) {
+                flag = true;
+                break;
+            }
+            first = first->next;
         }
-
-        first = first->next;
-        second = second->next;
+        first = this->head;
+        if (!flag) {
+            return flag;
+        }
     }
 
     return flag;
